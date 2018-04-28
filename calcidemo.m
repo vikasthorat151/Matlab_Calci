611bdeb5932edd36430327dbbf5b74fa41fb83e6
vikasthorat151 <vikasthorat151@gmail.com>
2018-04-28 13:14:17 +0530

    test1

---
+++ calcidemo.m
@@ -0,0 +1,210 @@
+function varargout = calcidemo(varargin)
+% CALCIDEMO MATLAB code for calcidemo.fig
+%      CALCIDEMO, by itself, creates a new CALCIDEMO or raises the existing
+%      singleton*.
+%
+%      H = CALCIDEMO returns the handle to a new CALCIDEMO or the handle to
+%      the existing singleton*.
+%
+%      CALCIDEMO('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in CALCIDEMO.M with the given input arguments.
+%
+%      CALCIDEMO('Property','Value',...) creates a new CALCIDEMO or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before calcidemo_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to calcidemo_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help calcidemo
+
+% Last Modified by GUIDE v2.5 09-Apr-2018 16:58:32
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @calcidemo_OpeningFcn, ...
+                   'gui_OutputFcn',  @calcidemo_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before calcidemo is made visible.
+function calcidemo_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to calcidemo (see VARARGIN)
+
+% Choose default command line output for calcidemo
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes calcidemo wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = calcidemo_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+
+function edit1_Callback(hObject, eventdata, handles)
+% hObject    handle to edit1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit1 as text
+%        str2double(get(hObject,'String')) returns contents of edit1 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function edit1_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function edit2_Callback(hObject, eventdata, handles)
+% hObject    handle to edit2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit2 as text
+%        str2double(get(hObject,'String')) returns contents of edit2 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function edit2_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function edit3_Callback(hObject, eventdata, handles)
+% hObject    handle to edit3 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit3 as text
+%        str2double(get(hObject,'String')) returns contents of edit3 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function edit3_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit3 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in pushbutton1.
+function pushbutton1_Callback(hObject, eventdata, handles)
+
+
+global value1;
+try
+    value1=str2num(handles.edit1.String);
+catch
+    value1=str2num(handles.edit1.String{1});
+end
+global value2;
+try
+    value2=str2num(handles.edit2.String);
+catch
+    value2=str2num(handles.edit2.String{1});
+end
+
+try
+    operation=handles.edit3.String;
+catch
+    operation=handles.edit3.String{1};
+end
+
+sfnew('-Mealy','democalci')
+add_block('Simulink/Sources/Constant','democalci/Constant','Position',[100 100 200 200],'Value','value1')
+add_block('Simulink/Sources/Constant','democalci/Constant','MakeNameUnique','on','Position',[100 250 200 350],'Value','value2') 
+
+
+ add_block('Simulink/Sinks/To Workspace','democalci/To Workspace','Position',[500 175 600 275]);
+ 
+ if(operation=='+')
+        add_block('Simulink/Math Operations/Add','democalci/Add','Position',[250 175 350 275]);
+        add_line('democalci',{'Constant/1','Constant1/1','Add/1'},{'Add/1','Add/2','To Workspace/1'});
+ end
+ 
+ if(operation=='-')
+        add_block('Simulink/Math Operations/Subtract','democalci/Subtract','Position',[250 175 350 275]);
+        add_line('democalci',{'Constant/1','Constant1/1','Subtract/1'},{'Subtract/1','Subtract/2','To Workspace/1'});
+ end
+ 
+  if(operation=='*')
+        add_block('Simulink/Math Operations/Product','democalci/Product','Position',[250 175 350 275]);
+        add_line('democalci',{'Constant/1','Constant1/1','Product/1'},{'Product/1','Product/2','To Workspace/1'});
+  end
+ 
+   if(operation=='/')
+        add_block('Simulink/Math Operations/Divide','democalci/Divide','Position',[250 175 350 275]);
+        add_line('democalci',{'Constant/1','Constant1/1','Divide/1'},{'Divide/1','Divide/2','To Workspace/1'});
+   end
+ 
+sim('democalci');
+global output1;
+%output1=evalin('base',simout);
+handles.text2.String=simout.Data(end);
+
+
+% hObject    handle to pushbutton1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --- Executes during object creation, after setting all properties.
+function text2_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to text2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
